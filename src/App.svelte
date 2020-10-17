aeb42a9227bf0362fc78dcc73d873da0cf99edc5
gretzkiy <dias7707@gmail.com>
2020-10-17 15:15:01 +0300

    Linting

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let name;
+</script>
+
+<style>
+	.demo {
+		color: blue;
+	}
+</style>
+
+<h1 class="demo">Hello {name}!</h1>
---
+++ src/profile.ts
@@ -0,0 +1,11 @@
+export interface Person {
+	name: string;
+	lastname: string;
+	age: number;
+}
+
+export const person: Person = {
+	name: 'Alex',
+	lastname: 'Korolev',
+	age: 22,
+};
