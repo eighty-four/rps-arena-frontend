aed52e58ecb3ed73edaaf678942f317c4e51fe67
Dikarev Andrey <dikarev@edadeal.ru>
2020-10-17 11:01:25 +0300

    Ts and Js compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    mode: 'development',
+	entry: path.resolve(__dirname, "src", "index.ts"),
+
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+
+	mode: "development",
 };
